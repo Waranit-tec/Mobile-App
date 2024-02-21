a0b62b2c179870a1ae8dd5ffb86ea9733329b3b9
Waranit-tec <v.varanid@gmail.com>
2024-02-21 15:42:38 +0700

    new

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+import Welcome from './screens/Welcome';
+import Ex01 from './screens/week2/Ex01';
+import Ex02 from './screens/week2/Ex02';
+import Ex03 from './screens/week2/Ex03';
+import Ex04 from './screens/week2/Ex04';
+import Ex05 from './screens/week2/Ex05';
+import Ex06 from './screens/week2/Ex06';
+import Ex07 from './screens/week2/Ex07';
+import Ex08 from './screens/week2/Ex08';
+import Ex09 from './screens/week2/Ex09';
+import Ex10 from './screens/week2/Ex10';
+import Ex11 from './screens/week2/Ex11';
+import Ex12 from './screens/week2/Ex12';
+import Extra from './screens/week2/Extra';
+import Travel from './screens/week3/Travel';
+import Resort from './screens/week3/Resort';
+import { TextInput, View, StyleSheet } from 'react-native';
+import Greeting from './components/Greeting';
+
+import { PaperProvider } from 'react-native-paper';
+import TestPaper from './screens/week4/TestPaper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <Ex01 />
+    // <Ex02 />
+    // <Ex03 />
+    // <Ex04 />
+    // <Ex05 />
+    // <Ex06 />
+    // <Ex07 />
+    // <Ex08 />
+    // <Ex09 />
+    // <Ex10 />
+    // <Ex11 />
+    // <Ex12 />
+    // <Extra />
+    // <View style={styles.container}>    
+    //    <TextInput
+    //     keyboardType="email-address"
+    //     autoCapitalize="none"
+    //     autoCorrect={false}
+    //     maxLength={30}
+    //     multiline={false}
+    //     placeholder="Please enter your email"
+    //     returnKeyType="next"
+    //     onChangeText={(text)=>{}}
+    //     defaultVaslue="Default text"
+    //   />
+    // </View>
+
+    // <View style={{alignItems: 'center', top: 50}}>
+    // <Greeting name='Rexxar' />
+    // <Greeting name='Jaina' />
+    // <Greeting name='Valeera' />
+    // </View>
+
+    // <Travel />
+
+    // <PaperProvider>
+    //   <TestPaper />
+    // </PaperProvider>
+
+    <Resort/>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ECD4D4',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
