3decd24564a7d80112c417e1678c6b43cfa8ba4e
Waranit-tec <v.varanid@gmail.com>
2024-04-03 11:13:49 +0700

    fixed

--- navigations/RootStack.js
+++ navigations/RootStack.js
@@ -4,6 +4,8 @@ import BottomTab from "./BottomTab";
 import PokemonTab from "./PokemonTab";
 import YourHealth from "../screens/week9/YourHealth";
 import TodoList from "../screens/week9/TodoList";
+import Location from "../screens/week10/Location";
+import LocationQuiz from "../screens/week10/LocationQuiz";
 
 const Stack = createStackNavigator();
 
@@ -14,6 +16,8 @@ export default function RootStack() {
       <Stack.Screen name="PokemonTab" component={PokemonTab} options={{ title : "Pokemon World" }}  />
       <Stack.Screen name="YourHealth" component={YourHealth} />
       <Stack.Screen name="TodoList" component={TodoList} />
+      <Stack.Screen name="Location" component={Location} />
+      <Stack.Screen name="LocationQuiz" component={LocationQuiz} />
     </Stack.Navigator>
   );
 }
